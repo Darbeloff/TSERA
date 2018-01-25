a18fe6085c53bed05da687b287c635a5d6207ffa
ashikari <ashikari123@gmail.com>
2018-01-25 12:22:01 -0500

    added .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashikari/TSERA/src
-# Build directory: /home/ashikari/TSERA/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(joy_stage)
-subdirs(kinematics)
--- build/kinematics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashikari/TSERA/src/kinematics
-# Build directory: /home/ashikari/TSERA/build/kinematics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
