5fd4b95b9b03c22980f82f16c5ad6ca33b0fd64f
ashikari <ashikari123@gmail.com>
2018-02-14 01:34:33 -0500

    minor changes/ clean up

--- src/sensor_test/sensor_test.ino
+++ src/sensor_test/sensor_test.ino
@@ -1,16 +1,14 @@
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(A0, INPUT);
-  pinMode(8,INPUT);
+  pinMode(8,INPUT_PULLUP);
 }
 
 int k = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
   k = digitalRead(8);
-  //k = analogRead(A0);
   Serial.println(k);
 }
