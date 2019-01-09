48b2e2079c710d3b44618333c44f76c56101d825
Gabriel Valdes <gvaldes@mit.edu>
2019-01-09 15:00:32 -0500

    Debugging joyInterpreter.py, determined updateXYZ calculation is incorrect, need to map xyz in joystick to stage.

--- src/joy_interpreter/src/joyInterpreter.py
+++ src/joy_interpreter/src/joyInterpreter.py
@@ -32,7 +32,8 @@ class commandClass:
 			self.y = 7*y
 		self.z = (92-173)*((z+1)/2)+173 #normalize axes 3 from 0 to 1, set Z to reverse scale of axes 3.
 	def updateCommand(self):
-		self.command = ik_legs(self.x,self.y,self.z)
+		#self.command = ik_legs(self.x,self.y,self.z)
+		self.command = [self.x,self.y,self.z]
 	def getCommand(self):
 		return self.command
 	def getStage(self):
@@ -40,7 +41,7 @@ class commandClass:
 
 
 
-command = [0]*9;
+command = [0]*10;
 # z =92
 # x= 0
 # y= 0
@@ -56,7 +57,7 @@ def axes_map(js_sp,ax_max,ax_min):
 
 def command_cb(msg):
 	global command
-
+	nav = 0
 	#Take joystick input to determine what navigation method to use
 	if msg.buttons[3]:
 		nav = 0
@@ -71,21 +72,21 @@ def command_cb(msg):
 			command3.updateZ(axes_map(msg.axes[3],z_max,z_min))
 			command3.updateCommand()
 		if msg.buttons[0]:
-			command3.updateXY(msg.axes[0],msg.axes[1],msg.axes[3])
+			command3.updateXY(-1*msg.axes[0],msg.axes[1],msg.axes[3])
 			command3.updateCommand()
 
 		if msg.buttons[9]:
 			command2.updateZ(axes_map(msg.axes[3],z_max,z_min))
 			command2.updateCommand()
 		if msg.buttons[1]:
-			command2.updateXY(msg.axes[0],msg.axes[1],msg.axes[3])
+			command2.updateXY(-1*msg.axes[0],msg.axes[1],msg.axes[3])
 			command2.updateCommand()
 
 		if msg.buttons[11]:
 			command1.updateZ(axes_map(msg.axes[3],z_max,z_min))
 			command1.updateCommand()
 		if msg.buttons[2]:
-			command1.updateXY(msg.axes[0],msg.axes[1],msg.axes[3])
+			command1.updateXY(-1*msg.axes[0],msg.axes[1],msg.axes[3])
 			command1.updateCommand()
 
 	elif nav == 1:
@@ -94,27 +95,24 @@ def command_cb(msg):
 		# same as above, X and Y are set by axes 0 and 1, Z is set by axes 3
 		# have to make sure X and Y are within a circle of radius 7
 		if msg.buttons[8]:
-			command3.updateXYZ(msg.axes[0],msg.axes[1],msg.axes[3])
+			command3.updateXYZ(-1*msg.axes[0],msg.axes[1],msg.axes[3])
 			command3.updateCommand()
 
 		if msg.buttons[9]:
-			command2.updateXYZ(msg.axes[0],msg.axes[1],msg.axes[3])
+			command2.updateXYZ(-1*msg.axes[0],msg.axes[1],msg.axes[3])
 			command2.updateCommand()
 
 		if msg.buttons[11]:
-			command1.updateXYZ(msg.axes[0],msg.axes[1],msg.axes[3])
+			command1.updateXYZ(-1*msg.axes[0],msg.axes[1],msg.axes[3])
 			command1.updateCommand()		
 
 	#elif nav == 2: 
 		# gradient descent
 
-	print command
 	command[0:3] = command1.getCommand()
-	print command
 	command[3:6] = command2.getCommand()
-	print command
-	command[6:] = command3.getCommand()
-	print command
+	command[6:9] = command3.getCommand()
+	command[9] = nav
 	command_msg = Float32MultiArray(data = command)
 	ik_pub.publish(command_msg)
 
