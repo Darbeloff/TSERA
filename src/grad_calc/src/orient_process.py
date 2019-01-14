e276e61bb67bb09b557d66fd500c3451eaffecf5
Gabriel Valdes <gvaldes@mit.edu>
2019-01-14 17:31:46 -0500

    building out gradient ascent, current issues is in saving iterations of command properly for smooth movement

--- src/grad_calc/src/orient_process.py
+++ src/grad_calc/src/orient_process.py
@@ -2,10 +2,17 @@
 import rospy
 from std_msgs.msg import Float32MultiArray
 import numpy as np
+from sympy import *
 #from ik_helper import *
 #from sensor_msgs.msg import Joy
 
 ort_pub = rospy.Publisher("/des_ort_xyz", Float32MultiArray, queue_size = 1)
+
+x, y, Lt = symbols('x y Lt')
+b_x = (sqrt((-6*y**2)+2*(sqrt(3)*Lt+3*x)*(-x+sqrt(x**2+y**2))))/(sqrt(Lt**2))
+b_y = ((x+sqrt(x**2+y**2))*sqrt(-6*y**2+2(sqrt(3)*Lt+3*x)*(-x+sqrt(x**2+y**2))))/(sqrt(Lt**2)*y)
+b_z = (Lt-2*sqrt(3)*sqrt(x**2+y**2))/(sqrt(Lt**2))
+
 
 class positionClass():
 	def __init__(self, stage_):
@@ -13,34 +20,72 @@ class positionClass():
 		self.y = 0
 		self.z = 92
 		self.count = 0
+		self.list = [0]
 	def updateXYZ(self,x,y,z):
 		self.x = x
 		self.y = y
 		self.z = z
+	def x(self):
+		return self.x
+	def y(self):
+		return self.y
+	def z(self):
+		return self.z
+	def updatelist(self, xyz):
+		if self.list[0] == 0:
+			self.list[0] = xyz
+		elif len(self.list) < 10
+			self.list.append(xyz)
+			self.count = len(self.list)
 	def updatepos(self):
 		self.count += 1
 	def position(self):
 		return self.count
+	def J(self, T, y):
+		if abs(y) > 0.1:
+			b_vector = [b_x, b_y, b_z]
+		else
+			b_y = -b_x*b_z
+			b_vector = [b_x, b_y, b_z]
+		J = np.dot(b_vector, T)
+		return J
+	def arrived(self):
+		self.list.pop(0)
+		xyz_msg = Float32MultiArray(data = self.list[0])
+		ort_pub.publish(xyz_msg)
 
+position = positionClass()
+
+def gradient_ascent(unit_vector):
+	
+	while position.position() < 10
+		x, y, Lt = symbols('x y Lt')
+		J = position.J(unit_vector, position.y())
+		djdx = diff(J, x)
+		djdy = diff(J, y)
+		new_x = position.x() + djdx.subs([(x, position.x()),(y,position.y()), (Lt, 85.3)])*alpha
+		new_y = position.y() + djdy.subs([(x,position.x()),(y,position.y()), (Lt, 85.3)])*alpha
+		new_z = 1 #This can't be correct
+		position.updateXYZ(new_x,new_y,new_z)
+		xyz = [position.x(),position.y(),position.z()]
+		position.updatelist(xyz)
+		if position.position() == 0:
+			xyz_msg = Float32MultiArray(data = xyz)
+			ort_pub.publish(xyz_msg)
 
 def ort_callback(msg):
 	xyz = [0]*3
 	magnitude = np.sqrt(((msg.data[0])**2) + ((msg.data[1])**2) + ((msg.data[2])**2))
 	unit_vector = [msg.data[0], msg.data[1], msg.data[2]]/magnitude
-	#unit vector msg
-	
-	#mathematica
-	Lt = 85.3 #mm update with value from spherical joints
-	s0 = 33.23 #mm
-	Ltv = (2*((Lt**2)-(Lt/2)**2)**.5)/3
-#Need current XYZ values to find sa,sb,sc,ha,hb,hc for NBT. 
+	gradient_ascent(unit_vector)
 
-	xyz_msg = Float32MultiArray(data = xyz)
-	ort_pub.publish(xyz_msg)
+def waypoint_callback(msg):
+	position.arrived()
 
 def orientation():
 	rospy.init_node('grad_calc')
 	rospy.Subscriber("/des_ort", Float32MultiArray, ort_callback)
+	rospy.Subscriber("/next_waypoint", Bool, waypoint_callback)
 	rospy.spin()
 
 if __name__ = '__main__':
