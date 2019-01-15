7682e53f176fa9d92ca9ad23d7a2e28ae01b016e
Gabriel Valdes <gvaldes@mit.edu>
2019-01-15 12:23:04 -0500

    catching back up to remote repository

---
+++ src/grad_calc/src/orient_process.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+import rospy
+from std_msgs.msg import Float32MultiArray, Bool
+import numpy as np
+from sympy import *
+#from ik_helper import *
+#from sensor_msgs.msg import Joy
+
+ort_pub = rospy.Publisher("/des_ort_xyz", Float32MultiArray, queue_size = 1)
+
+x, y, Lt = symbols('x y Lt')
+b_x = (sqrt((-6*y**2)+2*(sqrt(3)*Lt+3*x)*(-x+sqrt(x**2+y**2))))/(sqrt(Lt**2))
+b_y = ((x+sqrt(x**2+y**2))*sqrt(-6*y**2+2*(sqrt(3)*Lt+3*x)*(-x+sqrt(x**2+y**2))))/(sqrt(Lt**2)*y)
+b_z = (Lt-2*sqrt(3)*sqrt(x**2+y**2))/(sqrt(Lt**2))
+
+
+class positionClass():
+	def __init__(self):
+		self.x = 0
+		self.y = 0
+		self.z = 92
+		self.count = 0
+		self.list = [0]*100
+		self.grad_cont = True
+	def updateXYZ(self,x,y,z):
+		self.x = x
+		self.y = y
+		self.z = z
+	def x(self):
+		return self.x
+	def y(self):
+		return self.y
+	def z(self):
+		return self.z
+	def updatelist(self, xyz):
+		if self.list[0] == 0:
+			self.list[0] = xyz
+		elif self.list[-1] == 0:
+			self.list[self.count] = xyz
+	def updatepos(self):
+		self.count += 1
+	def position(self):
+		return self.count
+	def J(self, T, y):
+		if abs(y) > 0.1:
+			b_vector = [b_x, b_y, b_z]
+		else:
+			b_y = -b_x*b_z
+			b_vector = [b_x, b_y, b_z]
+		J = np.dot(b_vector, T)
+		return J
+	def arrived(self):
+		self.list.pop(0)
+		xyz_msg = Float32MultiArray(data = self.list[0])
+		ort_pub.publish(xyz_msg)
+	def cont(self):
+		return self.grad_cont
+	def clearlist(self):
+		self.list = [0]*100
+
+position = positionClass()
+alpha = 0.5
+
+def gradient_ascent(unit_vector):
+	position.grad_cont = True
+	x, y, Lt = symbols('x y Lt')
+	xyz = [0]*3
+	new_x = 1
+	new_y = 1
+	new_z = 1
+	J = position.J(unit_vector, position.y())
+	djdx = diff(J, x)
+	djdy = diff(J, y)
+	while position.cont() == True and sqrt((new_x-xyz[0])**2+(new_y-xyz[1])**2+(new_z-xyz[3])**2) > 0.1:
+		new_x = position.x() + djdx.subs([(x, position.x()),(y,position.y()), (Lt, 85.3)])*alpha
+		new_y = position.y() + djdy.subs([(x,position.x()),(y,position.y()), (Lt, 85.3)])*alpha
+		new_z = position.z() #This can't be correct
+		position.updateXYZ(new_x,new_y,new_z)
+		xyz = [position.x(),position.y(),position.z()]
+		xyz_msg = Float32MultiArray(data = xyz)
+		ort_pub.publish(xyz_msg)
+
+	if position.cont() == True:
+		new_x = position.x() + djdx.subs([(x, position.x()),(y,position.y()), (Lt, 85.3)])*0.1
+		new_y = position.y() + djdy.subs([(x,position.x()),(y,position.y()), (Lt, 85.3)])*0.1
+		new_z = position.z() #This can't be correct
+		position.updateXYZ(new_x,new_y,new_z)
+		xyz = [position.x(),position.y(),position.z()]
+		xyz_msg = Float32MultiArray(data = xyz)
+		ort_pub.publish(xyz_msg)		
+
+def ort_callback(msg):
+	position.grad_cont = False
+	xyz = [0]*3
+	magnitude = np.sqrt(((msg.data[0])**2) + ((msg.data[1])**2) + ((msg.data[2])**2))
+	unit_vector = [msg.data[0], msg.data[1], msg.data[2]]/magnitude
+	gradient_ascent(unit_vector)
+	
+
+def waypoint_callback(msg):
+	position.arrived()
+
+def pos_callback(msg):
+	position.updateXYZ(msg.data[0], msg.data[1], msg.data[2])
+
+def orientation():
+	rospy.init_node('grad_calc')
+	rospy.Subscriber("/des_ort", Float32MultiArray, ort_callback)
+	rospy.Subscriber("/continueWaypoint", Bool, waypoint_callback)
+	rospy.Subscriber("/des_pos", Float32MultiArray, pos_callback)
+	rospy.spin()
+
+if __name__ == '__main__':
+	orientation()
+
+
+#Need to create plotting section to map out the testing. 
---
+++ src/joy_interpreter/src/joyInterpreter.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+import rospy
+from std_msgs.msg import Float32MultiArray
+import numpy as np
+from ik_helper import *
+from sensor_msgs.msg import Joy
+
+ik_pub = rospy.Publisher('/des_pos',Float32MultiArray,queue_size = 1)
+
+
+max_radius = 7
+class commandClass:
+	def __init__(self,stage_):
+		self.stage = stage_
+		self.x = 0
+		self.y = 0
+		self.z = 92
+		self.command = ik_legs(self.x,self.y,self.z)
+		self.nav = 0
+	def updateXY(self,x,y,joy_rad):
+		drad = axes_map(joy_rad,max_radius,0)
+		r = np.sqrt(x**2+y**2)
+		self.x = drad*(x/r)
+		self.y = drad*(y/r)
+	def updateZ(self,z):
+		self.z = z
+	def updateXYZ(self,x,y,z):
+		r = np.sqrt(x**2+y**2) 
+		if r > 1:
+			self.x = 7*(x/r)
+			self.y = 7*(y/r)
+		else:
+			self.x = 7*x
+			self.y = 7*y
+		self.z = (92-173)*((z+1)/2)+173 #normalize axes 3 from 0 to 1, set Z to reverse scale of axes 3.
+	def updateNav(self,nav):
+		self.nav = nav
+	def getNav(self):
+		return self.nav
+	def updateCommand(self):
+		#self.command = ik_legs(self.x,self.y,self.z)
+		self.command = [self.x,self.y,self.z]
+	def getCommand(self):
+		return self.command
+	def getStage(self):
+		return self.stage
+
+command = [0]*10;
+# z =92
+# x= 0
+# y= 0
+
+z_min = 92
+z_max = 173
+
+def axes_map(js_sp,ax_max,ax_min):
+	sp = (js_sp+1)/2.0
+	out = (ax_max - ax_min)*sp+ax_min
+	return out
+
+def command_cb(msg):
+	global command
+	#Take joystick input to determine what navigation method to use
+	if msg.buttons[3]:
+		command1.updateNav(0)
+		command2.updateNav(0)
+		command3.updateNav(0)
+	elif msg.buttons[4]:
+		command1.updateNav(1)
+		command2.updateNav(1)
+		command3.updateNav(1)
+	elif msg.buttons[5]:
+		command1.updateNav(2)
+		command2.updateNav(2)
+		command3.updateNav(2)
+
+	if command1.getNav() == 0:
+		# XYZ
+		if msg.buttons[8]:
+			command3.updateZ(axes_map(msg.axes[3],z_max,z_min))
+			command3.updateCommand()
+		if msg.buttons[0]:
+			command3.updateXY(-1*msg.axes[0],msg.axes[1],msg.axes[3])
+			command3.updateCommand()
+
+		if msg.buttons[9]:
+			command2.updateZ(axes_map(msg.axes[3],z_max,z_min))
+			command2.updateCommand()
+		if msg.buttons[1]:
+			command2.updateXY(-1*msg.axes[0],msg.axes[1],msg.axes[3])
+			command2.updateCommand()
+
+		if msg.buttons[11]:
+			command1.updateZ(axes_map(msg.axes[3],z_max,z_min))
+			command1.updateCommand()
+		if msg.buttons[2]:
+			command1.updateXY(-1*msg.axes[0],msg.axes[1],msg.axes[3])
+			command1.updateCommand()
+
+	elif command1.getNav() == 1:
+		# Vector motion
+
+		# same as above, X and Y are set by axes 0 and 1, Z is set by axes 3
+		# have to make sure X and Y are within a circle of radius 7
+		if msg.buttons[8]:
+			command3.updateXYZ(-1*msg.axes[0],msg.axes[1],msg.axes[3])
+			command3.updateCommand()
+
+		if msg.buttons[9]:
+			command2.updateXYZ(-1*msg.axes[0],msg.axes[1],msg.axes[3])
+			command2.updateCommand()
+
+		if msg.buttons[11]:
+			command1.updateXYZ(-1*msg.axes[0],msg.axes[1],msg.axes[3])
+			command1.updateCommand()		
+
+	#elif nav == 2: 
+		# gradient descent
+
+	command[0:3] = command1.getCommand()
+	command[3:6] = command2.getCommand()
+	command[6:9] = command3.getCommand()
+	command[9] = command1.getNav()
+	command_msg = Float32MultiArray(data = command)
+	ik_pub.publish(command_msg)
+
+
+def ik():
+	print "Listening to joystick..."
+	rospy.init_node('joyinterpreter')
+	rospy.Subscriber('/joy', Joy, command_cb)
+	rospy.spin()
+
+if __name__ == '__main__':
+	command1 = commandClass(1)
+	command2 = commandClass(2)
+	command3 = commandClass(3)
+	ik()
+	
+
+
