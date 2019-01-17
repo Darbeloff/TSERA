Corrected joyInterpreter to publish unit vector with correct max tilt. Changing gradient ascent to include algebraic expression from mathematica for the differential of b

src/grad_calc/src/orient_process.py
@@ -2,54 +2,58 @@
 import rospy
 from std_msgs.msg import Float32MultiArray, Bool
 import numpy as np
-from sympy import *
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 ort_pub = rospy.Publisher("/des_ort_xyz", Float32MultiArray, queue_size = 1)
 
-x, y, Lt = symbols('x y Lt')
-b_x = (sqrt((-6*y**2)+2*(sqrt(3)*Lt+3*x)*(-x+sqrt(x**2+y**2))))/(sqrt(Lt**2))
-b_y = ((x+sqrt(x**2+y**2))*sqrt(-6*y**2+2*(sqrt(3)*Lt+3*x)*(-x+sqrt(x**2+y**2))))/(sqrt(Lt**2)*y)
-b_z = (Lt-2*sqrt(3)*sqrt(x**2+y**2))/(sqrt(Lt**2))
-
-
-class positionClass():
+class poseClass():
 	def __init__(self, stage_):
 		self.stage = stage_
 		self.x = 0
 		self.y = 0
 		self.z = 92
 		self.count = 0
-		self.list = [0]*100
+		self.b_list = [0]*100
 		self.grad_cont = True
 		self.T_vector = [0,0,0]
+		self.b_vector = [0,0,1]
 	def updateXYZ(self,x,y,z):
 		self.x = x
 		self.y = y
-		self.z = 92+(z/7*(173-92))
-	# def x(self):
-	# 	return self.x
-	# def y(self):
-	# 	return self.y
-	# def z(self):
-	# 	return self.z
+		self.z = z
 	def updatelist(self, xyz):
 		if self.list[0] == 0:
 			self.list[0] = xyz
 		elif self.list[-1] == 0:
 			self.list[self.count] = xyz
-	def updatepos(self):
+	def updatecount(self):
 		self.count += 1
 	def position(self):
 		return self.count
-	def J(self, T, y):
-		if abs(y) > 0.1:
-			b_vector = [b_x, b_y, b_z]
-		else:
-			b_vector = [b_x, -b_x*b_z, b_z]
-		print ('b_vector = ', b_vector)
-		J = np.dot(b_vector, T)
+	def J(self, T):
+		J = np.dot(self.b_vector, self.T_vector)
+		X = self.x
+		Y = self.y
+		djdx = (0.01504299815501891*(2*(33.238054997246756 + X)*(-1. + X/sqrt(X^2 + Y^2) + 2.*(-1.*X + sqrt(X**2 + Y**2)))/sqrt(-2*Y**2 + 2.*(33.238054997246756 + X)*(-1.*X + sqrt(X**2 + Y**2)) - \
+		0.000905167173967609* Sqrt[-2*Y^2 + 2.*(33.238054997246756 + X)*(-1.*X + Sqrt[X^2 + Y^2])] +\ 
+		0.000522598511551812* Sqrt[-6*Y^2 + 6.*(33.238054997246756 + X)*(-1.*X + Sqrt[X^2 + Y^2])] - \
+   		0.000905167173967609*Sqrt[X^2 + Y^2]*((0.5*(2.*(33.238054997246756 + X)*(-1. + X/Sqrt[X^2 + Y^2]) + 2.*(-1.*X + Sqrt[X^2 + Y^2])))/\
+   		Sqrt[-2*Y^2 + 2.*(33.238054997246756 + X)*(-1.*X + Sqrt[X^2 + Y^2])] - (0.4082482904638632*(3.*(33.238054997246756 + X)*(-1. + X/Sqrt[X^2 + Y^2]) + 3.*(-1.*X + Sqrt[X^2 + Y^2])))/\
+   		Sqrt[-3*Y^2 + 3.*(33.238054997246756 + X)*(-1.*X + Sqrt[X^2 + Y^2])]) - \
+   		(1/Sqrt[X^2 + Y^2])*0.000905167173967609*X*(1.*Sqrt[-2*Y^2 + 2.*(33.238054997246756 + X)*(-1.*X + Sqrt[X^2 + Y^2])] - 0.8164965809277264*\
+     	Sqrt[-3*Y^2 + \
+       	3.*(33.238054997246756 + X)*(-1.*X + Sqrt[X^2 + Y^2])]) + \
+   		X*(-((0.0004525835869838045*(2.*(33.238054997246756 + X)*(-1. + \
+        X/Sqrt[X^2 + Y^2]) + 2.*(-1.*X + Sqrt[X^2 + Y^2])))/\
+        \
+       	Sqrt[-2*Y^2 + \
+        2.*(33.238054997246756 + X)*(-1.*X + Sqrt[X^2 + Y^2])]) + \
+        (0.000261299255775906*(6.*(33.238054997246756 + X)*(-1. +\
+        X/Sqrt[X^2 + Y^2]) + 6.*(-1.*X + Sqrt[X^2 + Y^2])))/\
+        \
+     	Sqrt[-6*Y^2 + \
+       	6.*(33.238054997246756 + X)*(-1.*X + Sqrt[X^2 + Y^2])])\
 		return J
 	def arrived(self):
 		self.list.pop(0)
@@ -57,23 +61,39 @@ class positionClass():
 		ort_pub.publish(xyz_msg)
 	def cont(self):
 		return self.grad_cont
-	def clearlist(self):
-		self.list = [0]*100
-	def updateT(self, list):
-		self.T_vector = list
+	def updateT(self, vector):
+		self.b_list.clear()
+		self.T_vector = vector
 	def T(self):
 		return self.T_vector
+	def updateB(self, Lt):
+		if self.x == 0 and self.y == 0:
+			 self.b_vector = [0,0,1]
+		elif abs(self.y) > 0.1:
+			b_x = (sqrt((-6*self.y**2)+2*(sqrt(3)*Lt+3*self.x)*(-self.x+sqrt(self.x**2+self.y**2))))/(sqrt(Lt**2))
+			b_y = ((self.x+sqrt(self.x**2+self.y**2))*sqrt(-6*self.y**2+2*(sqrt(3)*Lt+3*self.x)*(-self.x+sqrt(self.x**2+self.y**2))))/(sqrt(Lt**2)*self.y)
+			b_z = (Lt-2*sqrt(3)*sqrt(self.x**2+self.y**2))/(sqrt(Lt**2))
+			self.b_vector = [b_x, b_y, b_z]
+		else:
+			b_x = (sqrt((-6*self.y**2)+2*(sqrt(3)*Lt+3*self.x)*(-self.x+sqrt(self.x**2+self.y**2))))/(sqrt(Lt**2))
+			b_z = (Lt-2*sqrt(3)*sqrt(self.x**2+self.y**2))/(sqrt(Lt**2))
+			self.b_vector = [b_x, -b_x*b_z, b_z]
+		self.list.append(b_vector)
+	def b_vec(self):
+		return self.b_vector
 
 alpha = 0.5
 
 def gradient_ascent(stage, unit_vector):
 	if stage == 1:
-		position = pos1
+		pose = pose1
 	elif stage == 2:
-		position = pos2
+		pose = pose2
 	else:
-		position = pos3
-	position.grad_cont = True
+		pose = pose3
+	
+	pose.updateT(unit_vector)
+	pose.grad_cont = True
 	x, y, Lt = symbols('x y Lt')
 	
 	xyz = [0]*9
@@ -82,18 +102,15 @@ def gradient_ascent(stage, unit_vector):
 	new_y = 1
 	new_z = 1
 
-	J = position.J(unit_vector, position.y)
-	print ('J =', J)
-
-	djdx = diff(J, x)
-	djdy = diff(J, y)
-	print ('djdx = ', djdx) 
-	# while position.cont() == True and sqrt((new_x-xyz[0])**2+(new_y-xyz[1])**2+(new_z-xyz[3])**2) > 0.1:
-	# 	new_x = position.x + djdx.subs([(x, position.x),(y,position.y), (Lt, 85.3)])*alpha
-	# 	new_y = position.y + djdy.subs([(x,position.x),(y,position.y), (Lt, 85.3)])*alpha
-	# 	new_z = position.z #This can't be correct
-	# 	position.updateXYZ(new_x,new_y,new_z)
-	# 	vector_plot = np.array([[0,0,0,b_x.subs([(x, position.x),(y,position.y), (Lt, 85.3)]), b_y.subs([(x, position.x),(y,position.y), (Lt, 85.3)]), b_z.subs([(x, position.x),(y,position.y), (Lt, 85.3)])],[0, 0, 0, unit_vector[0], unit_vector[1], unit_vector[2]]])
+	J = pose.J(unit_vector)
+	
+	while pose.cont() == True and sqrt((new_x-xyz[0])**2+(new_y-xyz[1])**2+(new_z-xyz[3])**2) > 0.1:
+	 	new_x = 
+	 	new_y = 
+	 	new_z = position.z 
+	 	position.updateXYZ(new_x,new_y,new_z)
+	 	
+	 	#vector_plot = np.array([[0,0,0,b_x.subs([(x, position.x),(y,position.y), (Lt, 85.3)]), b_y.subs([(x, position.x),(y,position.y), (Lt, 85.3)]), b_z.subs([(x, position.x),(y,position.y), (Lt, 85.3)])],[0, 0, 0, unit_vector[0], unit_vector[1], unit_vector[2]]])
 
 		# X, Y, Z, U, V, W = zip(*vector_plot)
 		# fig = plt.figure()
@@ -108,36 +125,32 @@ def gradient_ascent(stage, unit_vector):
 		# plt.show()
 		# plt.hold(True)
 
-		xyz = [pos1.x,pos1.y,pos1.z, pos2.x,pos2.y,pos2.z, pos3.x,pos3.y,pos3.z]
-		xyz_msg = Float32MultiArray(data = xyz)
-		ort_pub.publish(xyz_msg)		
+	xyz = [pose1.x,pose1.y,pose1.z, pose2.x,pose2.y,pose2.z, pose3.x,pose3.y,pose3.z]
+	xyz_msg = Float32MultiArray(data = xyz)
+	ort_pub.publish(xyz_msg)		
 
 def ort_callback(msg):
-	xyz = [0]*9
-	mag1 = np.sqrt(((msg.data[0])**2) + ((msg.data[1])**2) + ((msg.data[2])**2))
-	mag2 = np.sqrt(((msg.data[3])**2) + ((msg.data[4])**2) + ((msg.data[5])**2))
-	mag3 = np.sqrt(((msg.data[6])**2) + ((msg.data[7])**2) + ((msg.data[8])**2))
-	T1 = [msg.data[0], msg.data[1], msg.data[2]]/mag1
-	T2 = [msg.data[3], msg.data[4], msg.data[5]]/mag2
-	T3 = [msg.data[6], msg.data[7], msg.data[8]]/mag3
+	T1 = [msg.data[0], msg.data[1], msg.data[2]]
+	T2 = [msg.data[3], msg.data[4], msg.data[5]]
+	T3 = [msg.data[6], msg.data[7], msg.data[8]]
 	print ('T3 = ', T3)
-	if (pos1.T()!=T1).any():
+	if (pose1.T()!=T1).any():
 		gradient_ascent(1, T1)
-		pos1.T = T1
-	elif (pos2.T()!=T2).any():
+		pose1.updateT(T1)
+	elif (pose2.T()!=T2).any():
 		gradient_ascent(2, T2)
-		pos2.T = T2
-	elif (pos3.T()!=T3).any():
+		pose2.updateT(T2)
+	elif (pose3.T()!=T3).any():
 		gradient_ascent(3, T3)
-		pos3.T = T3
+		pose3.updateT(T3)
 	
 # def waypoint_callback(msg):
 # 	pos1.arrived()
 
 def pos_callback(msg):
-	pos1.updateXYZ(msg.data[0], msg.data[1], msg.data[2])
-	pos2.updateXYZ(msg.data[3], msg.data[4], msg.data[5])
-	pos3.updateXYZ(msg.data[6], msg.data[7], msg.data[8])
+	pose1.updateXYZ(msg.data[0], msg.data[1], msg.data[2])
+	pose2.updateXYZ(msg.data[3], msg.data[4], msg.data[5])
+	pose3.updateXYZ(msg.data[6], msg.data[7], msg.data[8])
 
 def orientation():
 	rospy.init_node('grad_calc')
@@ -147,7 +160,7 @@ def orientation():
 	rospy.spin()
 
 if __name__ == '__main__':
-	pos1 = positionClass(1)
-	pos2 = positionClass(2)
-	pos3 = positionClass(3)
+	pose1 = poseClass(1)
+	pose2 = poseClass(2)
+	pose3 = poseClass(3)
 	orientation()

src/joy_interpreter/src/joyInterpreter.py
@@ -24,15 +24,19 @@ class commandClass:
 		self.y = drad*(y/r)
 	def updateZ(self,z):
 		self.z = z
-	def updateXYZ(self,x,y,z):
+	def updateXYZ(self,x,y,slide):
+		print slide
+		phi = ((slide+1)/2)*(np.radians(55))
+		print phi
 		r = np.sqrt(x**2+y**2) 
-		if r > 1:
-			self.x = 7*(x/r)
-			self.y = 7*(y/r)
-		else:
-			self.x = 7*x
-			self.y = 7*y
-		self.z = (-7)*((z+1)/2)+7 #normalize axes 3 from 0 to 1, set Z to reverse scale of axes 3.
+		self.z = np.cos(phi)
+		r_corr = np.sin(phi)
+		scale = r_corr/r
+		print r
+		print self.z
+		print r_corr
+		self.x = scale*x
+		self.y = scale*y
 	def updateNav(self,nav):
 		self.nav = nav
 	def getNav(self):