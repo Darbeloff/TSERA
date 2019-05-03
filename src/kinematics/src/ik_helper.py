e3e4ef2498b2610cd2ded69b03f3991bbc034149
Gabriel Valdes <gvaldes@mit.edu>
2019-05-02 20:31:59 -0400

    catching up with repository

--- src/kinematics/src/ik_helper.py
+++ src/kinematics/src/ik_helper.py
@@ -29,8 +29,8 @@ def L_c(x, y, z, Lt, s0, r, M, k):
 
 
 def constrain_leg_width(rotations):
-	rot_min = 0; #revolutions
-	rot_max = 43.13/4.0 #revolutions
+	rot_min = 0.0; #revolutions
+	rot_max = 55.0; #revolutions, approx value
 	if rotations >= rot_max:
 		return rot_max
 	elif rotations<=rot_min:
@@ -38,9 +38,9 @@ def constrain_leg_width(rotations):
 	else:
 		return rotations
 
-lo = 30
-le = 42.5  + 85 + 42.5+ 30 # sum of all scissor lengths (minus offset)
-lb = 4 # length of offset
+lo = 42.0
+le = 5*42.0 # sum of all scissor lengths (minus offset)
+lb = 4.0 # length of offset
 
 def width(L_model):
 	ball_joint_offset =15 #mm measured
@@ -50,12 +50,11 @@ def width(L_model):
 	#Lmax = 175.33
 	#Lmin = 91.433
 
-	w = 2/(lb**2+le**2)*( (lb**2 + lo*le)*np.sqrt(lb**2+le**2-L**2) - L*lb*(le-lo) ) #mm
-
+	w = 2.0/(lb**2+le**2)*( (lb**2 + lo*le)*np.sqrt(lb**2+le**2-L**2) - L*lb*(le-lo) ) #mm 
 	#convert to revolutions of the motor shaft
-	lead = (1.0/20)*25.4 #mm/revolution of the motor output shaft
-	gap = 67.47 #mm from CAD
-	hinge_hole_to_outer_edge = 8.54 ## mm measured value
+	lead = 1.0 #mm/revolution of the motor output shaft
+	gap = 90.4 #mm from CAD
+	hinge_hole_to_outer_edge = 4.5 ## mm measured value
 	width_max = gap - 2*(hinge_hole_to_outer_edge) 
 	#width_min = 23 #mm measured value
 	revolutions = (width_max -  w)/(2*lead)
@@ -65,14 +64,14 @@ def width(L_model):
 
 
 
-def ik_legs(x,y,z):
+def ik_legs(x,y,z,rotated):
 	'''
 	takes x,y,z coordinate in segment base frame and 
 	calculates required leg lengths
 
 	'''
 	Lt = 85.3 #mm fill in new values with spherical joint
-	s0 = 33.23 #mm
+	s0 = 70 #mm
 
 	r = np.sqrt(x**2 + y**2)
 	la_L = L_a( x,y, z, Lt, s0, r)
@@ -87,13 +86,17 @@ def ik_legs(x,y,z):
 		M = np.sqrt(2*(r - x)*(np.sqrt(3)*Lt + 3*x) - 6.0*y**2.0)
 		lb_L = L_b(x, y, z, Lt, s0, r, M, k)
 		lc_L = L_c(x, y, z, Lt, s0, r, M, k)
+		#use simpler kinematic equations
 	else:
 		k = np.sqrt(-3*y**2 + (np.sqrt(3)*Lt + 3*x)*(-x + r))
 		M = np.sqrt(2*(r - x)*(np.sqrt(3)*Lt + 3*x) - 6*y**2)
 		lb_L = L_b(x, y, z, Lt, s0, r, M, k)
 		lc_L = L_c(x, y, z, Lt, s0, r, M, k)
-
-	legs = [width(la_L), width(lb_L), width(lc_L)]
+	print la_L, lb_L, lc_L
+	if rotated == 1:
+		legs = [width(lc_L), width(la_L), width(lb_L)]
+	else:
+		legs = [width(la_L), width(lb_L), width(lc_L)]
 	return legs
 
 
--- src/motorCmd/motorCmd.ino
+++ src/motorCmd/motorCmd.ino
@@ -4,6 +4,7 @@
 #include <ros.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
 
 //instantiate motors!
 //Portescap Motors
@@ -20,24 +21,25 @@ motorClass m1b =  motorClass(9,32,48,45,gearRatio2, EncCntsRev2);
 motorClass m1c =  motorClass(11,30,46,47,gearRatio2, EncCntsRev2);
 
 //stage 2
-motorClass m2a =  motorClass(7,34,38,41,gearRatio1, EncCntsRev1);
-motorClass m2b =  motorClass(6,35,42,39,gearRatio1, EncCntsRev1);
-motorClass m2c =  motorClass(4,37,40,43,gearRatio1, EncCntsRev1);
+motorClass m2a =  motorClass(7,34,38,41,gearRatio2, EncCntsRev2);
+motorClass m2b =  motorClass(6,35,42,39,gearRatio2, EncCntsRev2);
+motorClass m2c =  motorClass(4,37,40,43,gearRatio2, EncCntsRev2);
 
 //stage 3
-motorClass m3a =  motorClass(2,29,22,25,gearRatio1, EncCntsRev1); //1-1
-motorClass m3b =  motorClass(3,28,24,23,gearRatio1, EncCntsRev1); //1-2
-motorClass m3c =  motorClass(10,31,26,27,gearRatio1, EncCntsRev1); //2-1
+motorClass m3a =  motorClass(2,29,22,25,gearRatio2, EncCntsRev2); //1-1
+motorClass m3b =  motorClass(3,28,24,23,gearRatio2, EncCntsRev2); //1-2
+motorClass m3c =  motorClass(10,31,26,27,gearRatio2, EncCntsRev2); //2-1
 
 
 
 int vel_or_pos = 1; //position control
 int calibrated = 0;//initially not calibrated
-float squareError[3];
+float epsilon = 0.01;
 
-void command_callback(const std_msgs::Float32MultiArray& setpoint){
 
-    m1a.setMotorPos(setpoint.data[0]);
+void command_callback(const std_msgs::Float32MultiArray &setpoint){
+
+    m1a.setMotorPos(setpoint.data[0]); //width
     m1b.setMotorPos(setpoint.data[1]);
     m1c.setMotorPos(setpoint.data[2]);
   
@@ -53,30 +55,31 @@ void command_callback(const std_msgs::Float32MultiArray& setpoint){
 
 //ros
 bool ROS_switch = true;
+bool Error_switch = true;
 ros::NodeHandle ArduinoInterface;
 std_msgs::Float32MultiArray outputVelocity;
-std_msgs::Float32 squareError_msg1;
-//std_msgs::Float32 squareError_msg2;
-//std_msgs::Float32 squareError_msg3;
+std_msgs::Float32MultiArray squareError_msg;
+std_msgs::Bool ready_next;
 
 //create publisher
-//ros::Publisher squareErrorPub1("squareError1", &squareError_msg1);
-//ros::Publisher squareErrorPub2("squareError2", &squareError_msg2);
-//ros::Publisher squareErrorPub3("squareError3", &squareError_msg3);
-
+ros::Publisher error_check("continueWaypoint", &ready_next);
+ros::Publisher squareErrorPub("squareError", &squareError_msg);
 
 //subscriber
 ros::Subscriber<std_msgs::Float32MultiArray> velSub("ik", &command_callback);
+float array_storage[9];
 
 void setup () {
 
   if (ROS_switch){
     //initialize ros
     ArduinoInterface.initNode();
-    
+    squareError_msg.data_length = 9;
+    squareError_msg.data = array_storage;
 
     //publisher
-    //ArduinoInterface.advertise(squareErrorPub1);
+    ArduinoInterface.advertise(squareErrorPub);
+    ArduinoInterface.advertise(error_check);
 
     
     //subscriber
@@ -84,7 +87,7 @@ void setup () {
     delay(1000);
   }
   else{Serial.begin(9600);
-  m3a.setMotorPos(0);
+  m3c.setMotorPos(0);
   }
 
 }
@@ -116,31 +119,28 @@ void loop (){
 //    m1b.pos_closedLoopController();
 //    m1c.pos_closedLoopController();
 
-//    squareError[0] = pow(m1a.errorPos,2)+ pow(m1b.errorPos,2)+ pow(m1c.errorPos,2);
-//    squareError[1] = pow(m2a.errorPos,2)+ pow(m2b.errorPos,2)+ pow(m2c.errorPos,2);
-//    squareError[2] = pow(m3a.errorPos,2)+ pow(m3b.errorPos,2)+ pow(m3c.errorPos,2);
-    
-
-    
-    if(ROS_switch){
-      //squareError_msg1.data = squareError[0]+squareError[1]+squareError[2];
-      //squareError_msg2.data = squareError[1];
-      //squareError_msg3.data = 1; //squareError[2];
-      
-      //squareErrorPub1.publish( &squareError_msg1 );
-      //squareErrorPub2.publish( &squareError_msg2 );
-      //squareErrorPub3.publish( &squareError_msg3 );
+//potentially have it send only once per waypoint?
+      if(ROS_switch && Error_switch){ //Error Switch is set to stage 
+        if ((sqrt(pow(m3a.errorPos,2)+ pow(m3b.errorPos,2)+ pow(m3c.errorPos,2)))< epsilon && (sqrt(pow(m3a.errorPos,2)+ pow(m3b.errorPos,2)+ pow(m3c.errorPos,2)))> 0){
+            ready_next.data = true;
+            error_check.publish( &ready_next );
+          }
+          
+      squareError_msg.data[0] = m3a.errorPos;
+      squareError_msg.data[1] = m3b.errorPos; //first test, read encoder for all three, c was not responsive and A and B kept flickering back and forth.
+      squareError_msg.data[2] = m3c.errorPos; 
+      squareError_msg.data[3] = m3a.errorPos;
+      squareError_msg.data[4] = m3b.errorPos; //first test, read encoder for all three, c was not responsive and A and B kept flickering back and forth.
+      squareError_msg.data[5] = m3c.errorPos; 
+      squareError_msg.data[6] = m3a.errorPos;
+      squareError_msg.data[7] = m3b.errorPos; //first test, read encoder for all three, c was not responsive and A and B kept flickering back and forth.
+      squareError_msg.data[8] = m3c.errorPos; 
+      squareErrorPub.publish( &squareError_msg );
       ArduinoInterface.spinOnce();
-    }
+      delay(100);
+        }
     else{
-    m3a.log_on_off();
+    m3c.log_on_off();
     }
   }
-
-
-
-
-
-
-
 
